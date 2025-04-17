c3b9dc4520ed3f1621bfda973f493a9ce21aa725
sharath0036 <ksharathbabu2@gmail.com>
2025-04-17 18:10:36 +0530

    Masters Fixed

--- src/Masters/SourceCode/Js/pages/ItemNamesDelete/FormLoad/BuildBsTable/onClickRow/FetchDelete/AfterFetch/EntryFile.js
+++ src/Masters/SourceCode/Js/pages/ItemNamesDelete/FormLoad/BuildBsTable/onClickRow/FetchDelete/AfterFetch/EntryFile.js
@@ -1,5 +1,7 @@
 let StartFunc = () => {
-    window.location.href = "";
+    let jVarLocalHtmlId = 'RefreshBSTableId';
+    let jVarLocalRefreshBSTableId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalRefreshBSTableId.click();
 };
 
 export { StartFunc }
